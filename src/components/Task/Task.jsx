544040c8da4cf3bc49d6479dbc1311321b17b31d
StrixWL <brahimstrix@gmail.com>
2024-03-19 13:24:42 +0000

    optimized rendering

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,19 +1,47 @@
 import './Task.css'
 import RoundCheckbox from '../ui/RoundCheckbox/RoundCheckbox'
 import EditButton from '../ui/EditButton/EditButton'
+import { useEffect, useState } from 'react'
 
 const Task = (props) => {
+    const [isBeingEdited, setIsBeingEdited] = useState(props.item.isBeingEdited)
+    const [done, setDone] = useState(props.item.done)
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        console.log("rendered")
+    })
+    const onBlur = () => {
+        props.item.title = props.item.ref.current.value
+        setIsBeingEdited(false)
+    }
+    const onTaskEdit = () => {
+        setIsBeingEdited(true)
+        props.item.focus()
+    }
+    const onInputChange = () => {
+        props.item.done = !props.item.done
+        setDone(!done)
+    }
+    useEffect(() => {
+        props.item.focus()
+    }, [isBeingEdited])
     return (
         <div key={props.i} className='tasks__task'>
-            <RoundCheckbox id={props.i} checked={props.item.done} className='tasks__task-checkbox' onChange={() => props.onInputChange(props.i)} />
-            <input ref={props.item.ref} style={{
-                display: props.item.isBeingEdited ? 'inline': 'none',
-            }} onBlur={() => props.onBlur(props.i)} defaultValue={props.item.title} className='tasks__task-input' type="text" />
-            <span style={{
-                display: props.item.isBeingEdited ? 'none': 'inline',
-                textDecoration: props.item.done ? 'line-through' : 'none'
-            }} className='tasks__task-title'>{props.item.title}</span>
-            <EditButton className='tasks__task-edit-btn' onClick={() => props.onTaskEdit(props.i)} />
+            <RoundCheckbox id={props.i} checked={props.item.done} className='tasks__task-checkbox' onChange={() => onInputChange()} />
+            <input ref={props.item.ref} 
+                style={{
+                    display: isBeingEdited ? 'inline': 'none',
+                }}
+                onBlur={() => onBlur(props.i)} defaultValue={props.item.title} className='tasks__task-input' type="text" />
+            <span
+                onClick={() => setCount(count + 1)}
+                style={{
+                    display: isBeingEdited ? 'none': 'inline',
+                    textDecoration: props.item.done ? 'line-through' : 'none'
+                }} className='tasks__task-title'>{props.item.title}
+            </span>
+            <span className='tasks__task-counter'> {count} </span>
+            <EditButton className='tasks__task-edit-btn' onClick={() => onTaskEdit(props.i)} />
         </div>
     )
 }
--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,17 +1,11 @@
 import './Tasks.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import useFocus from '../../hooks/useFocus';
 import Task from '../../components/Task/Task';
 
 const Tasks = () => {
     const [items, setItems] = useState([])
-    useEffect(() => {
-        items.forEach(item => {
-            if (item.isBeingEdited)
-                item.focus() // bad
-        })
-    }, [items])
-    const onBtnclick = (id) => {
+    const onBtnclick = () => {
         const _items = [...items]
         const [inputRef, setInputFocus] = useFocus()
         _items.push({
@@ -23,28 +17,12 @@ const Tasks = () => {
         })
         setItems(_items)
     }
-    const onInputChange = (id) => {
-        const _items = [...items]
-        _items[id].done = !_items[id].done
-        setItems(_items)
-    }
-    const onBlur = (id) => {
-        const _items = [...items]
-        _items[id].isBeingEdited = false
-        _items[id].title = _items[id].ref.current.value
-        setItems(_items)
-    }
-    const onTaskEdit = (id) => {
-        const _items = [...items]
-        _items[id].isBeingEdited = true
-        setItems(_items)
-    }
     return (
         <div className='tasks'>
             <h1>My Tasks</h1>
             <button onClick={onBtnclick} className='tasks__add-btn'>Add a task</button>
             {items.map((item, i) => (
-                <Task key={i} item={item} i={i} onBlur={onBlur} onTaskEdit={onTaskEdit} onInputChange={onInputChange} />
+                <Task key={i} item={item} i={i} />
             ))}
         </div>
     )
